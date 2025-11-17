a9d2c36970c3d361577567f19c75b58bdd59599f
Cream.BR <creambr6@gmail.com>
2025-11-17 15:22:36 -0300

    Update MusicBeatSubState.hx

--- source/MusicBeatSubState.hx
+++ source/MusicBeatSubState.hx
@@ -2,6 +2,10 @@ package;
 
 import Conductor.BPMChangeEvent;
 import flixel.FlxSubState;
+import flixel.FlxCamera;
+import mobile.MobileControls;
+import mobile.flixel.FlxVirtualPad;
+import flixel.util.FlxDestroyUtil;
 
 class MusicBeatSubState extends FlxSubState
 {
@@ -15,6 +19,79 @@ class MusicBeatSubState extends FlxSubState
 	private var timeSinceLastBeat:Float = 1000;
 
 	private var countSteps:Bool = true;
+
+	public var mobileControls:MobileControls;
+	public var virtualPad:FlxVirtualPad;
+
+	public var vpadCam:FlxCamera;
+	public var camControls:FlxCamera;
+
+	public function addVirtualPad(DPad:FlxDPadMode, Action:FlxActionMode)
+	{
+		if (virtualPad != null)
+			removeVirtualPad();
+
+		virtualPad = new FlxVirtualPad(DPad, Action);
+		add(virtualPad);
+	}
+
+	public function removeVirtualPad()
+	{
+
+		if (virtualPad != null)
+			remove(virtualPad);
+	}
+
+	public function addMobileControls(DefaultDrawTarget:Bool = false)
+	{
+		mobileControls = new MobileControls();
+
+		camControls = new FlxCamera();
+		camControls.bgColor.alpha = 0;
+		FlxG.cameras.add(camControls, DefaultDrawTarget);
+
+		mobileControls.cameras = [camControls];
+		mobileControls.visible = false;
+		add(mobileControls);
+	}
+
+	public function removeMobileControls()
+	{
+
+		if (mobileControls != null)
+			remove(mobileControls);
+	}
+
+	public function addVirtualPadCamera(DefaultDrawTarget:Bool = false)
+	{
+		if (virtualPad != null)
+		{
+			vpadCam = new FlxCamera();
+			FlxG.cameras.add(vpadCam, DefaultDrawTarget);
+			vpadCam.bgColor.alpha = 0;
+			virtualPad.cameras = [vpadCam];
+		}
+	}
+
+	override function destroy()
+	{
+
+		//controls.isInSubstate = false;
+		
+		if (virtualPad != null)
+		{
+			virtualPad = FlxDestroyUtil.destroy(virtualPad);
+			virtualPad = null;
+		}
+
+		if (mobileControls != null)
+		{
+			mobileControls = FlxDestroyUtil.destroy(mobileControls);
+			mobileControls = null;
+		}
+		
+		super.destroy();
+	}
 
 	override function create(){
 		super.create();
