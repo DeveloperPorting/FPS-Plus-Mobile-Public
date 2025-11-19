a17f5e944996300b8f091019024c41864572615e
Cream.BR <creambr6@gmail.com>
2025-11-19 11:34:11 -0300

    Update ScriptedState.hx

--- source/scripts/ScriptedState.hx
+++ source/scripts/ScriptedState.hx
@@ -7,6 +7,14 @@ import modding.PolymodHandler;
 import extensions.flixel.FlxUIStateExt;
 import restricted.RestrictedUtils;
 
+#if mobile
+import flixel.FlxCamera;
+import mobile.MobileControls;
+import mobile.flixel.FlxVirtualPad;
+import flixel.util.FlxDestroyUtil;
+import flixel.FlxG;
+#end
+
 //Basically just FlxUIStateExt with polymodReload
 class ScriptedState extends FlxUIStateExt
 {
@@ -27,4 +35,75 @@ class ScriptedState extends FlxUIStateExt
 		Reflect.setProperty(r, "_stateName", stateString);
 		return r;
 	}
+
+	#if mobile
+	public var mobileControls:MobileControls;
+	public var virtualPad:FlxVirtualPad;
+
+	public var vpadCam:FlxCamera;
+	public var camControls:FlxCamera;
+
+	
+    public function addVirtualPad(DPad:FlxDPadMode, Action:FlxActionMode)
+	{
+		if (virtualPad != null)
+			removeVirtualPad();
+
+		virtualPad = new FlxVirtualPad(DPad, Action);
+		add(virtualPad);
+	}
+
+	public function removeVirtualPad()
+	{
+		if (virtualPad != null)
+			remove(virtualPad);
+	}
+
+	public function addMobileControls(DefaultDrawTarget:Bool = false)
+	{
+		mobileControls = new MobileControls();
+
+		camControls = new FlxCamera();
+		camControls.bgColor.alpha = 0;
+		FlxG.cameras.add(camControls, DefaultDrawTarget);
+
+		mobileControls.cameras = [camControls];
+		mobileControls.visible = false;
+		add(mobileControls);
+	}
+
+	public function removeMobileControls()
+	{
+		if (mobileControls != null)
+			remove(mobileControls);
+	}
+
+	public function addVirtualPadCamera(DefaultDrawTarget:Bool = false)
+	{
+		if (virtualPad != null)
+		{
+			vpadCam = new FlxCamera();
+			FlxG.cameras.add(vpadCam, DefaultDrawTarget);
+			vpadCam.bgColor.alpha = 0;
+			virtualPad.cameras = [vpadCam];
+		}
+	}
+
+	override function destroy()
+	{
+		super.destroy();
+
+		if (virtualPad != null)
+		{
+			virtualPad = FlxDestroyUtil.destroy(virtualPad);
+			virtualPad = null;
+		}
+
+		if (mobileControls != null)
+		{
+			mobileControls = FlxDestroyUtil.destroy(mobileControls);
+			mobileControls = null;
+		}
+	}
+	#end
 }
