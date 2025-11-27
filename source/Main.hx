658076f6eb1c92dc8f918560bfb61e516e5c7e69
Cream.BR <creambr6@gmail.com>
2025-11-27 20:31:14 -0300

    Replace mods directory check with mod copying

--- source/Main.hx
+++ source/Main.hx
@@ -34,10 +34,8 @@ class Main extends Sprite
 		if (!MobileUtil.areAssetsCopied("assets/"))
 			MobileUtil.copyAssetsFromAPK("assets/");
 
-		if (sys.FileSystem.exists("mods"))
-            sys.FileSystem.createDirectory("mods");
-
-        sys.io.File.saveContent("mods/put-mods-here.txt", "");
+		if (!MobileUtil.areModsCopied("mods/"))
+			MobileUtil.copyModsFromAPK("mods/");
 		#end
 			
 		PolymodHandler.init();
